328c56d91ad57fc7cdee58a0cf0bdb95b0f6fb94
rflechner <rflechner@romcyber.com>
2015-06-29 18:18:47 +0200

    TinyRest server is working on IOS

---
+++ TinyRest/TinyRest.iOS/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿module TinyRest.iOS.AssemblyInfo
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("TinyRest.iOS")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("TinyRest.iOS")>]
+[<assembly: AssemblyCopyright("Copyright ©  2015")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
+
---
+++ TinyRest/TinyRest.iOS/TinyRestServerCSharp.fs
@@ -0,0 +1,77 @@
+﻿namespace TinyRestServerCSharp
+
+    open TinyRestServerPCL
+    open TinyRestServer
+    open System.Collections.Generic
+    open System.Net
+    open System.Runtime.CompilerServices
+
+    type HttpRestServer (schema, port, basePath, routes, logger) =
+        member x.Listen () =
+            let listener = new Listener()
+            let conf = { Schema=schema; Port=port; BasePath=Some basePath; Routes=routes |> Seq.toList; Logger=logger }
+            listener |> listen conf
+
+    type ServerBuilder() =
+        let mutable port:int = 8001
+        let mutable basePath:string = "/"
+        let mutable schema = Http
+        let mutable logger:ILogger option = None
+
+        let routes = new List<HttpRoute>()
+
+        let toHttpReply (o:obj) = 
+            match o with
+            | null               -> text ""
+            | :? IHttpReply as r -> r
+            | :? string as r     -> text r
+            | _                  -> text (o.ToString())
+
+        member x.WithLogger p = 
+            logger <- Some(p)
+            x
+        member x.WithPort p = 
+            port <- p
+            x
+        member x.WithBasePath p = 
+            basePath <- p
+            x
+        member x.WithHttp () = 
+            schema <- Http
+            x
+        member x.WithHttps () = 
+            schema <- Https
+            x
+
+        member x.OnGetPattern (p, a:System.Func<IHttpRequest, IHttpResponse, obj>) =
+            let r = GET (Regex(p)) <| fun q r -> a.Invoke(q,r) |> toHttpReply
+            routes.Add r
+            x
+
+        member x.OnPostPattern (p, a:System.Func<IHttpRequest, IHttpResponse, obj>) =
+            let r = POST (Regex(p)) <| fun q r -> a.Invoke(q,r) |> toHttpReply
+            routes.Add r
+            x
+
+        member x.OnGetPath (p, a:System.Func<IHttpRequest, IHttpResponse, obj>) =
+            let r = GET (Path(p)) <| fun q r -> a.Invoke(q,r) |> toHttpReply
+            routes.Add r
+            x
+
+        member x.OnPostPath (p, a:System.Func<IHttpRequest, IHttpResponse, obj>) =
+            let r = POST (Path(p)) <| fun q r -> a.Invoke(q,r) |> toHttpReply
+            routes.Add r
+            x
+
+        member x.Create () =
+            new HttpRestServer(schema, port, basePath, routes, logger)
+
+    type TinyRest () =
+        static member Server() =
+            new ServerBuilder()
+
+//    [<Extension>]
+//    type HttpListenerResponseExtensions () =
+//         [<Extension>]
+//         static member inline Json (r:IHttpResponse, o:obj) = Newtonsoft.Json.JsonConvert.SerializeObject(o) |> text
+
